f184171026f362095ade50e272b1444229f66c9f
RAKIBUL <rakibulmd933@gmail.com>
2023-06-03 02:18:06 +0600

    [ADD]class name change

--- components/Header.js
+++ components/Header.js
@@ -1,6 +1,7 @@
 import React from 'react'
 
 function Header() {
+
 	return (
 		<div className="min-h-screen hero" style={{
 			backgroundImage: `url("/assets/cool-background.png")`,
@@ -8,14 +9,18 @@ function Header() {
 			height: '100%',
 		}} >
 			<div className="hero-overlay bg-opacity-60"></div>
-			<div className="items-center justify-center text-center text-neutral-content">
+			<div className="items-center justify-center my-12 text-center text-neutral-content">
 				<div className="w-full">
 					<button className="my-12 btn btn-primary">Get Started</button>
-					<h1 className="mb-5 font-mono font-normal uppercase text-8xl">Join Into Your</h1>
-					<h1 className="mb-5 font-mono font-normal uppercase text-8xl"> Favorite</h1>
-					<h1 className="mb-5 font-mono font-normal uppercase text-8xl">Games</h1>
+					<h1 className="mb-5 font-mono font-normal uppercase text-8xl text-shadow text-shadow-red">Join Into Your</h1>
+					<h1 className="mb-5 font-mono font-normal uppercase text-8xl text-shadow text-shadow-red "> Favorite</h1>
+					<h1 className="mb-5 font-mono font-normal uppercase text-8xl text-shadow text-shadow-red hover:text-shadow-lg hover:text-purple-500">Games</h1>
 				</div>
 			</div>
+			<div className="">
+				<h2 className="">About</h2>
+			</div>
+
 		</div>
 	)
 }
--- components/Navbar.js
+++ components/Navbar.js
@@ -5,10 +5,10 @@ function Navbar() {
 		<div className="navbar">
 			<div className="navbar-start">
 				<div className="dropdown">
-					<label tabindex="0" className="btn btn-ghost lg:hidden">
-						<svg xmlns="http://www.w3.org/2000/svg" className="w-5 h-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path stroke-linecap="round" stroke-linejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
+					<label tabIndex="0" className="btn btn-ghost lg:hidden">
+						<svg xmlns="http://www.w3.org/2000/svg" className="w-5 h-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
 					</label>
-					<ul tabindex="0" className="p-2 mt-3 shadow menu menu-sm dropdown-content bg-base-100 rounded-box w-52">
+					<ul tabIndex="0" className="p-2 mt-3 shadow menu menu-sm dropdown-content bg-base-100 rounded-box w-52">
 						<li><a>Top Gamers</a></li>
 						<li>
 							<a>Games</a>
