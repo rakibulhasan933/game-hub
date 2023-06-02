02eb0390d180ccb4698694a2f682315d222030a9
RAKIBUL <rakibulmd933@gmail.com>
2023-06-02 16:42:20 +0600

    [ADD]class name change

--- components/Navbar.js
+++ components/Navbar.js
@@ -2,13 +2,13 @@ import React from 'react'
 
 function Navbar() {
 	return (
-		<div class="navbar">
-			<div class="navbar-start">
-				<div class="dropdown">
-					<label tabindex="0" class="btn btn-ghost lg:hidden">
+		<div className="navbar">
+			<div className="navbar-start">
+				<div className="dropdown">
+					<label tabindex="0" className="btn btn-ghost lg:hidden">
 						<svg xmlns="http://www.w3.org/2000/svg" className="w-5 h-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
 					</label>
-					<ul tabindex="0" class="menu menu-sm dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52">
+					<ul tabindex="0" className="menu menu-sm dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52">
 						<li><a>Top Gamers</a></li>
 						<li>
 							<a>Games</a>
@@ -16,10 +16,10 @@ function Navbar() {
 						<li><a>Live Games</a></li>
 					</ul>
 				</div>
-				<a class="btn btn-ghost normal-case text-xl">Gamer Hub</a>
+				<a className="btn btn-ghost normal-case text-xl">Gamer Hub</a>
 			</div>
-			<div class="navbar-center hidden lg:flex">
-				<ul class="menu menu-horizontal px-1">
+			<div className="navbar-center hidden lg:flex">
+				<ul className="menu menu-horizontal px-1">
 					<li><a>Top Gamers</a></li>
 					<li>
 						<a>Games</a>
@@ -27,8 +27,8 @@ function Navbar() {
 					<li><a>Live Games</a></li>
 				</ul>
 			</div>
-			<div class="navbar-end">
-				<a class="btn">Login</a>
+			<div className="navbar-end">
+				<a className="btn">Login</a>
 			</div>
 		</div>
 	)
