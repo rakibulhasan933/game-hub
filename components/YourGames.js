2c0163255661b0b4d0a0af6cfbeb90e1bdd78ecb
RAKIBUL <rakibulmd933@gmail.com>
2023-06-05 02:20:29 +0600

    [ADD] part

--- components/YourGames.js
+++ components/YourGames.js
@@ -9,6 +9,7 @@ import "swiper/css/pagination";
 import Image from "next/image";
 
 function YourGames() {
+
 	return (
 		<div className="py-20">
 			<Swiper
@@ -25,28 +26,28 @@ function YourGames() {
 				className="mySwiper"
 			>
 				<SwiperSlide>
-					<Image className="object-cover rounded " src='/assets/select your games/Mask group (1).png' alt="game" width={340} height={375} />
+					<Image className="object-cover w-full rounded " src='/assets/select your games/Mask group (1).png' alt="game" width={340} height={375} />
 				</SwiperSlide>
 				<SwiperSlide>
-					<Image className="object-cover rounded " src='/assets/select your games/Mask group (1).png' alt="game" width={340} height={375} />
+					<Image className="object-cover rounded " src='/assets/select your games/Mask group (2).png' alt="game" width={340} height={375} />
 				</SwiperSlide>
 				<SwiperSlide>
-					<Image className="object-cover rounded " src='/assets/select your games/Mask group (1).png' alt="game" width={340} height={375} />
+					<Image className="object-cover rounded " src='/assets/select your games/Mask group (3).png' alt="game" width={340} height={375} />
 				</SwiperSlide>
 				<SwiperSlide>
-					<Image className="object-cover rounded " src='/assets/select your games/Mask group (1).png' alt="game" width={340} height={375} />
+					<Image className="object-cover rounded " src='/assets/select your games/Mask group (4).png' alt="game" width={340} height={375} />
 				</SwiperSlide>
 				<SwiperSlide>
-					<Image className="object-cover rounded " src='/assets/select your games/Mask group (1).png' alt="game" width={340} height={375} />
+					<Image className="object-cover rounded " src='/assets/select your games/Mask group (5).png' alt="game" width={340} height={375} />
 				</SwiperSlide>
 				<SwiperSlide>
-					<Image className="object-cover rounded " src='/assets/select your games/Mask group (1).png' alt="game" width={340} height={375} />
+					<Image className="object-cover rounded " src='/assets/select your games/Mask group (6).png' alt="game" width={340} height={375} />
 				</SwiperSlide>
 				<SwiperSlide>
-					<Image className="object-cover rounded " src='/assets/select your games/Mask group (1).png' alt="game" width={340} height={375} />
+					<Image className="object-cover rounded " src='/assets/select your games/Mask group (7).png' alt="game" width={340} height={375} />
 				</SwiperSlide>
 				<SwiperSlide>
-					<Image className="object-cover rounded " src='/assets/select your games/Mask group (1).png' alt="game" width={340} height={375} />
+					<Image className="object-cover rounded " src='/assets/select your games/Mask group (8).png' alt="game" width={340} height={375} />
 				</SwiperSlide>
 			</Swiper>
 		</div>
