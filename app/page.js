b5965091bbf554f41e9986da9e71db8edb1968ab
RAKIBUL <rakibulmd933@gmail.com>
2023-06-05 02:46:40 +0600

    [ADD] file uploader

--- app/page.js
+++ app/page.js
@@ -1,5 +1,6 @@
 import GameHub from "@/components/GameHub";
 import Header from "@/components/Header";
+import SelectedBanner from "@/components/SelectedBanner";
 import YourGames from "@/components/YourGames";
 
 
@@ -9,6 +10,7 @@ export default function Home() {
       <div className="mx-20">
         <Header />
         <GameHub />
+        <SelectedBanner />
         <YourGames />
       </div>
     </main>
